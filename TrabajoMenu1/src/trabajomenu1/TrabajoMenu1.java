2ac414617886ae31de4a95275b33aa022e6a678d
Usuario <Usuario@DESKTOP-T1EGAER>
2019-10-25 17:03:07 -0500

    agregando segundo menu y operaciones basicas de la calculadora

--- TrabajoMenu1/src/trabajomenu1/TrabajoMenu1.java
+++ TrabajoMenu1/src/trabajomenu1/TrabajoMenu1.java
@@ -19,6 +19,38 @@ public class TrabajoMenu1 {
 
            switch(opcion){
                case 1:
+                   int numero1 = 3;
+                   int numero2 = 4;
+                   int i=1;
+                while(i==1){
+                int opc = Integer.parseInt(JOptionPane.showInputDialog(null,"SELECCIONE UNA OPCION\n"+
+                                                                            "1. SUMA\n"+
+                                                                            "2. RESTA\n"+
+                                                                            "3. MULTIPLICACION\n"+
+                                                                            "4. DIVISION\n"+
+                                                                            "5. VOLVER"));
+
+                switch (opc){
+                    case 1:
+                        JOptionPane.showMessageDialog(null, obj.sumar(numero1,numero2));
+                        break;
+                    case 2: 
+                        JOptionPane.showMessageDialog(null, obj.restar(numero1,numero2));
+                        break;
+                    case 3:
+                        JOptionPane.showMessageDialog(null, obj.multiplicar(numero1,numero2));
+                        break;
+                    case 4:
+                        JOptionPane.showMessageDialog(null, obj.division(numero1,numero2));
+                        break;
+                    case 5:
+                        i=0;
+                        JOptionPane.showMessageDialog(null, "VOLVIENDO");
+                        break;
+                    default:
+                        JOptionPane.showMessageDialog(null, "opcion no valida");
+                    }     
+                }     
                    break;
                    
                case 2:
